19a1ce6bcba4060f3f5667ebce8188f28f4de4bd
oott123 <git@public.oott123.com>
2020-09-13 14:29:25 +0800

    chore: use shared assembly info

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("20.9.13.0")]
+[assembly: AssemblyFileVersion("20.9.13.0")]
