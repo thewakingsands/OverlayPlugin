9469dfa02519510bccf043e8dc3baf9f039fbd05
oott123 <git@public.oott123.com>
2020-09-16 14:07:36 +0800

    chore: bump version to 20.9.16.0

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -2,5 +2,5 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("20.9.13.0")]
-[assembly: AssemblyFileVersion("20.9.13.0")]
+[assembly: AssemblyVersion("20.9.16.0")]
+[assembly: AssemblyFileVersion("20.9.16.0")]
