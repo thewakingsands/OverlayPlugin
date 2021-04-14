f7cd59dd2bbfc62ba14c34333885580ee9640ca1
oott123 <git@public.oott123.com>
2021-04-14 18:38:20 +0800

    chore: bump version to 21.4.14.0

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -2,5 +2,5 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("21.4.6.0")]
-[assembly: AssemblyFileVersion("21.4.6.0")]
+[assembly: AssemblyVersion("21.4.14.0")]
+[assembly: AssemblyFileVersion("21.4.14.0")]
