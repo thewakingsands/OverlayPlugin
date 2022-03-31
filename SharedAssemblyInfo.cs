bb7431d1ba1053486d094843fe14032a25ae19cf
oott123 <git@public.oott123.com>
2022-03-31 14:46:18 +0800

    bump version

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -2,5 +2,5 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("22.3.31.0")]
-[assembly: AssemblyFileVersion("22.3.31.0")]
+[assembly: AssemblyVersion("22.3.31.1")]
+[assembly: AssemblyFileVersion("22.3.31.1")]
