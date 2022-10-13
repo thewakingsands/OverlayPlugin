f20283397fd4f282da84399b176d9f330b4f3d89
sandtechnology <a1294790523@hotmail.com>
2022-10-13 19:18:27 +0800

    Bump to 22.10.13.0

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -2,5 +2,5 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("22.10.12.2")]
-[assembly: AssemblyFileVersion("22.10.12.2")]
+[assembly: AssemblyVersion("22.10.13.0")]
+[assembly: AssemblyFileVersion("22.10.13.0")]
