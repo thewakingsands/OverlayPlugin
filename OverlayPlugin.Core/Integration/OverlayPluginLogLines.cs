4a880bf563e5a25b21b4093a7f127b94f0404701
sandtechnology <a1294790523@hotmail.com>
2022-12-20 01:40:50 +0800

    Add missed LineInCombat log line

--- OverlayPlugin.Core/Integration/OverlayPluginLogLines.cs
+++ OverlayPlugin.Core/Integration/OverlayPluginLogLines.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using RainbowMage.OverlayPlugin.MemoryProcessors.InCombat;
 using RainbowMage.OverlayPlugin.Updater;
 
 namespace RainbowMage.OverlayPlugin.NetworkProcessors
@@ -15,6 +16,7 @@ namespace RainbowMage.OverlayPlugin.NetworkProcessors
             container.Register(new LineMapEffect(container));
             container.Register(new LineFateControl(container));
             container.Register(new LineCEDirector(container));
+            container.Register(new LineInCombat(container));
         }
     }
 
