69fdfa3115a6f3b36cd6e2a0a80345b4a21353b0
sandtechnology <a1294790523@hotmail.com>
2023-01-31 00:32:06 +0800

    Update to 6.20c and Bump version

--- OverlayPlugin.Core/GameRepoInfo.cs
+++ OverlayPlugin.Core/GameRepoInfo.cs
@@ -10,7 +10,7 @@ namespace RainbowMage.OverlayPlugin
     {
         //CN 6.20
         public static readonly Version version = new Version(6, 2);
-        public static readonly int CEDirectorOpcode = 0x08C;
-        public static readonly int MapEffectOpcode = 0x309;
+        public static readonly int CEDirectorOpcode = 0x0E4;
+        public static readonly int MapEffectOpcode = 0x24B;
     }
 }
--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -2,5 +2,5 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("22.12.20.0")]
-[assembly: AssemblyFileVersion("22.12.20.0")]
+[assembly: AssemblyVersion("23.1.30.1")]
+[assembly: AssemblyFileVersion("23.1.30.1")]
