9c8f5a55db82e5cc3941b1383c760e45945a5e94
sandtechnology <a1294790523@hotmail.com>
2023-08-08 15:37:47 +0800

    cn 6.38

--- OverlayPlugin.Core/GameRepoInfo.cs
+++ OverlayPlugin.Core/GameRepoInfo.cs
@@ -8,10 +8,10 @@ namespace RainbowMage.OverlayPlugin
 {
     public class GameRepoInfo
     {
-        //CN 6.31
+        //CN 6.38
         public static readonly Version version = new Version(6, 3);
-        public static readonly int CEDirectorOpcode = 0x3E6;
-        public static readonly int MapEffectOpcode = 0x30E;
-        public static readonly int RSVDataOpcode = 0x30F;
+        public static readonly int CEDirectorOpcode = 0x068;
+        public static readonly int MapEffectOpcode = 0x0CD;
+        public static readonly int RSVDataOpcode = 0x1C8;
     }
 }
