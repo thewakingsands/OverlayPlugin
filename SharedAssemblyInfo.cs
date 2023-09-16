130a283fb4880a16acd266254fb52ffc95c3df16
sandtechnology <a1294790523@hotmail.com>
2023-09-16 18:46:10 +0800

    cn 6.38

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -2,5 +2,5 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("23.7.11.0")]
-[assembly: AssemblyFileVersion("23.7.11.0")]
+[assembly: AssemblyVersion("23.8.8.0")]
+[assembly: AssemblyFileVersion("23.8.8.0")]
